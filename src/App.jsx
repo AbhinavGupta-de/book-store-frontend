c23b7cf98ca41c7fd0c465e1c02f7cfe097eecb7
Abhinav Gupta <Abhinav.gupta@outlook.de>
2023-10-26 20:51:51 +0530

    Completed the routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import CreateBooks from './pages/CreateBooks';
+import EditBooks from './pages/EditBooks';
+import ShowBooks from './pages/ShowBooks';
+import DeleteBook from './pages/DeleteBook';
+import NotFound from './pages/NotFound';
+
 function App() {
 	return (
-		<>
-			<div>
-				<div>
-					<h1>Hello World</h1>
-				</div>
-			</div>
-		</>
+		<Routes>
+			<Route path="/" element={<Home />} />
+		</Routes>
 	);
 }
 
